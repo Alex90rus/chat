6282a62ac8d56694cfd4c0dc06c57cbaf1b1d9d0
Alex90rus <alex90prus@yandex.ru>
2020-06-20 15:49:39 +0300

    It`s alive

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,58 @@
-import React from 'react';
+import React, { useEffect, useReducer } from 'react';
+import axios from 'axios';
+import socket from './socket';
 import JoinBlock from './components/JoinBlock';
+import reducer from './reducer';
+import Chat from './components/Chat';
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, {
+    joined: false,
+    roomId: null,
+    userName: null,
+    users: [],
+    messages: [],
+  });
+
+  const setUsers = (users) => {
+    dispatch({
+      type: 'SET_USERS',
+      payload: users,
+    });
+  };
+
+  const onLogin = async (obj) => {
+    dispatch({
+      type: 'JOINED',
+      payload: obj,
+    });
+    socket.emit('ROOM:JOIN', obj);
+    const { data } = await axios.get(`/rooms/${obj.roomId}`);
+    dispatch({
+      type: 'SET_DATA',
+      payload: data,
+    });
+  };
+
+  const addMessage = (message) => {
+    dispatch({
+      type: 'NEW_MESSAGE',
+      payload: message,
+    });
+  };
+
+  useEffect(() => {
+    socket.on('ROOM:SET_USERS', setUsers);
+    socket.on('ROOM:NEW_MESSAGE', addMessage);
+  }, []);
+
+  window.socket = socket;
+
   return (
     <div className="wrapper">
-      <JoinBlock />
+      {!state.joined
+        ? <JoinBlock onLogin={onLogin} />
+        : <Chat state={state} onAddMessage={addMessage} />}
     </div>
   );
 }
--- src/components/JoinBlock.jsx
+++ src/components/JoinBlock.jsx
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import socket from '../socket';
 
-function JoinBlock() {
+function JoinBlock({ onLogin }) {
+  const [roomId, setRoomId] = useState('');
+  const [userName, setUserName] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onEnter = async () => {
+    if (!roomId || !userName) {
+      return console.log('Введите верные данные');
+    }
+    const obj = {
+      roomId,
+      userName,
+    };
+    setIsLoading(true);
+    await axios.post('/rooms', obj);
+    return onLogin(obj);
+  };
+
   return (
     <div>
       <div className="join-block">
-        <input type="text" placeholder="Room ID" />
-        <input type="text" placeholder="Ваше имя" />
-        <button type="button" className="btn btn-success">ВОЙТИ</button>
+        <input
+          type="text"
+          placeholder="Room ID"
+          value={roomId}
+          onChange={(event) => setRoomId(event.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Ваше имя"
+          value={userName}
+          onChange={(event) => setUserName(event.target.value)}
+        />
+        <button disabled={isLoading} onClick={onEnter} type="button" className="btn btn-success">
+          {isLoading ? 'ВХОД...' : 'ВОЙТИ'}
+        </button>
       </div>
     </div>
   );
